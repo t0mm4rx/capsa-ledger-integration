494c6f3ea1dd5f2d14680fc5f8efa3ab1c84a83a
Tom Marx <tom.marx@hotmail.fr>
2022-08-30 12:02:33 +0200

    Unit tests

--- tests/src/test.fixture.js
+++ tests/src/test.fixture.js
@@ -28,7 +28,7 @@ const NANOSP_PLUGIN_PATH = Resolve('elfs/plugin_nanosp.elf');
 const NANOX_PLUGIN_PATH = Resolve('elfs/plugin_nanox.elf');
 
 const nano_models: DeviceModel[] = [
-    { name: 'nanos', letter: 'S', path: NANOS_PLUGIN_PATH, eth_path: NANOS_ETH_PATH },
+    // { name: 'nanos', letter: 'S', path: NANOS_PLUGIN_PATH, eth_path: NANOS_ETH_PATH },
     // { name: 'nanosp', letter: 'SP', path: NANOSP_PLUGIN_PATH, eth_path: NANOSP_ETH_PATH },
     { name: 'nanox', letter: 'X', path: NANOX_PLUGIN_PATH, eth_path: NANOX_ETH_PATH }
 ];
